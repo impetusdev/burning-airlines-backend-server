baf77644d1c9798213363d5c58a1eb7b02103f10
karmalexz <49818239+karmalexz@users.noreply.github.com>
2022-02-15 22:48:56 +1100

    edit rb files under models

--- burning-airlines-backend-server/app/models/plane.rb
+++ burning-airlines-backend-server/app/models/plane.rb
@@ -1,2 +1,6 @@
 class Plane < ApplicationRecord
+    has_many :flights 
+    validates :name, presence: true, uniqueness: true
+    validates :rows, presence: true
+    validates :cols, presence: true
 end
--- burning-airlines-backend-server/app/models/reservation.rb
+++ burning-airlines-backend-server/app/models/reservation.rb
@@ -1,2 +1,29 @@
 class Reservation < ApplicationRecord
+  belongs_to :flight, optional: true 
+  belongs_to :user, optional: true
+
+
+  validates :rows, presence: true
+  validates :seat, presence: true, format: { with: /\A[A-Z]\d+\z/, message: "format incorrect. Please enter seat number in the correct format, e.g. A1." }
+  validates_uniqueness_of :seat, scope: [:flight_id] # checks if seat is taken
+  validate :seat_limits
+
+  private
+
+  def seat_limits
+    unless flight_id == nil
+      row = seat[0].ord - 64
+      col = seat[1..seat.size].to_i
+      plane_rows = self.flight.plane.rows
+      plane_cols = self.flight.plane.cols
+
+      if row > plane_rows
+        errors.add(:seat, "row doesn't exist. Please capitalize the letter or check the number of rows on the plane for this flight.")
+      elsif col < 1
+        errors.add(:seat, "column doesn't exist.")
+      elsif col > plane_cols
+        errors.add(:seat, "column doesn't exist. Check the number of columns on the plane for this flight.")
+      end
+    end
+  end
 end
